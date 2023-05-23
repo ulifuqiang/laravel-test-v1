6f41651624e4533c7562ad83c14ca71d2499efe2
hui <hye@live.ca>
2023-05-23 19:47:25 +0800

    add a test .

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::get('/api/count', 'CounterController@getCount');
 
 // 更新计数，自增或者清零
 Route::post('/api/count', 'CounterController@updateCount');
+
+// new api getway for test...
+Route::get('/api/test', function () {
+    return 'this is a test!';
+});
